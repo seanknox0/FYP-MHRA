77f7f7adb8b002cab20aca24727ad36e9f0ec473
seanknox0 <seanknox0@gmail.com>
2022-01-31 12:23:05 +0000

    First commit. Login, Signup and profile page UI created.

---
+++ app/src/main/java/ie/wit/mhra/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package ie.wit.mhra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
