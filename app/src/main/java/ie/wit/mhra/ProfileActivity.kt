04d59364509aae9e0e556f2afec09e3702db1fba
seanknox0 <seanknox0@gmail.com>
2022-02-03 22:19:43 +0000

    Login/SignUp system complete. Profile page began, shows user logged in currently.

--- app/src/main/java/ie/wit/mhra/ProfileActivity.kt
+++ app/src/main/java/ie/wit/mhra/ProfileActivity.kt
@@ -1,11 +1,55 @@
 package ie.wit.mhra
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.firebase.auth.FirebaseAuth
+import ie.wit.mhra.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    //ViewBinding
+    private lateinit var binding: ActivityProfileBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configure ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "Profile"
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, logout
+        binding.logoutBtn.setOnClickListener{
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        //check user is logged in or not
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            //user not null, user is logged in, get user info
+            val email = firebaseUser.email
+            //set to text view
+            binding.emailTv.text = email
+        }
+        else {
+            //user is null, user is not logged in, go to login activity
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
